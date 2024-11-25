eb94839287f2939f15a1c3c33fdb8b797283a22b
unknown <mariateresamaedj19@gmail.com>
2024-11-26 01:31:31 +0800

    Is able ask user for input.

---
+++ program_num1.py
@@ -0,0 +1,11 @@
+'''
+Program 1:
+Create a program that ask user for personal information. Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc. Write the collected information in a txt file. It's up to you on how you'd like to format the information in the file. The program should ask user if want to input another person or exit.
+'''
+
+name = input("What is your Name? ")
+age = int(input("How old are you? "))
+birthday = (input("When is your birthday? "))
+city = input("Which City do you live in? ")
+course = input("What course are you studying currently? ")
+pet = input("Do you have a pet? ")
