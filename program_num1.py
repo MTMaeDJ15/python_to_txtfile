c6d6819cadf515321d5c9e967b603ce14aeffa5d
unknown <mariateresamaedj19@gmail.com>
2024-11-30 15:55:30 +0800

    questions completed and loop works well

--- program_num1.py
+++ program_num1.py
@@ -3,36 +3,49 @@ Program 1:
 Create a program that ask user for personal information. Minimum of 5 information per person, more info the better. eg. Fullname, Address, etc. Write the collected information in a txt file. It's up to you on how you'd like to format the information in the file. The program should ask user if want to input another person or exit.
 '''
 
-name = input("What is your Name? ")
+def data():
+    information = [] 
+    while True:
+        name = input("What is your Name? ")
+        while True:    
+            age = int(input("How old are you? "))
+            if len(str(age)) <= 2:
+                break
+            else: 
+                len(str(age)) >= 2
+                print(" Please Input Two Digit Number! ")
 
-while True:    
-    age = int(input("How old are you? "))
-    if len(str(age)) <= 2:
-        break
-    else: 
-        len(str(age)) >= 2
-        print(" Please Input Two Digit Number! ")
+        birthday = (input("When is your birthday? "))
+        address = input("Which City and Province do you live in? ")
+        course = input("What course are you studying currently? ")
 
-birthday = (input("When is your birthday? "))
-address = input("Which City and Province do you live in? ")
-course = input("What course are you studying currently? ")
+        while True: 
+            pet = input("Do you have a pet?(y/n) ")
+            if pet == "y":
+                pet_type = input("What type of Pet? ")
+                break
+            else: 
+                pet != "y"    
+                break
 
-while True: 
-    pet = input("Do you have a pet?(y/n) ")
-    if pet == "y":
-        pet_type = input("What type of Pet? ")
-        break
-    else: 
-        pet != "y"    
-        break
+        while True:
+            hobby = input("Do you have a hobby?(y/n) ")
+            if hobby == "y":
+                hobby_kind = input("What kind of Hobby? ")
+                break
+            else: 
+                hobby != "y"
+                break
 
-while True:
-    hobby = input("Do you have a hobby?(y/n) ")
-    if hobby == "y":
-        hobby_kind = input("What kind of Hobby? ")
-        break
-    else: 
-        hobby != "y"
-        break
+        information.append({"name":name, "age":age, "birthday": birthday, "address":address, "course":course, "pet":pet, "hobby":hobby})
 
-print("Thank you for answering! <3 ")
+        retry = input("Do you want to enter another set of data?(y/n) ").lower()
+
+        if retry != "y": 
+            break
+
+    return information
+
+mainloop = data
+
+mainloop()
