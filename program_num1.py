40474fd74820bb792422f7d7a308178fbb4ca6ce
unknown <mariateresamaedj19@gmail.com>
2024-12-01 15:07:53 +0800

    polished a bit more, i added comments and a retry function for program 2

--- program_num1.py
+++ program_num1.py
@@ -5,21 +5,21 @@ Create a program that ask user for personal information. Minimum of 5 informatio
 
 def data():
     information = [] 
-    while True:
+    while True: #This is the first loop for the whole program.
         name = input("What is your Name?(Sn, Fn M.I) ")
-        while True:    
+        while True: #This is the second loop for the age input.
             age = int(input("How old are you? "))
-            if len(str(age)) <= 2:
+            if len(str(age)) <= 2: #The inputted age should be 2 digit or below 2. 
                 break
             else: 
-                len(str(age)) >= 2
+                len(str(age)) >= 2 #This is the remind the user to input a valid number for his/her age.
                 print(" Please Input a Valid Age Number! ")
 
         birthday = (input("When is your birthday? "))
         address = input("Where do you live in? ")
         course = input("What course are you studying currently? ")
 
-        while True: 
+        while True: #This is the third loop for pet input, it ask a y/n question first before the detailed answer.
             pet = input("Do you have a pet?(y/n) ")
             if pet == "y":
                 pet_type = input("What type of Pet? ")
@@ -28,7 +28,7 @@ def data():
                 pet != "y"    
                 break
 
-        while True:
+        while True: #This is the fourth loop for hobby input, similar with pet input - it asks a y/n question before the detailed one.
             hobby = input("Do you have a hobby?(y/n) ")
             if hobby == "y":
                 hobby_kind = input("What kind of Hobby? ")
@@ -37,6 +37,7 @@ def data():
                 hobby != "y"
                 break
 
+        #This part will append or add the inputted information to the list above.
         information.append({
             "Name":name, 
             "Age":age, 
@@ -47,6 +48,7 @@ def data():
             "Hobby":hobby_kind
             })
 
+        #This part will ask the user if he/she wants to input another set of data.
         retry = input("Do you want to enter another set of data?(y/n) ").lower()
 
         if retry != "y": 
@@ -54,6 +56,7 @@ def data():
 
     return information
 
+#Main will be the whole program, it contains the above part and also will handle in transfering the data to the txt file.
 def main():
     information = data()
 
--- program_num2.py
+++ program_num2.py
@@ -3,7 +3,7 @@ Program 2:
 Create another program that ask user for fullname. Find the full name in the txt file (output of program1). Display the informations found in the txt file.
 '''
 def search_in_file():
-    name_to_search = input("Enter the Name(Sn, Fn M.I) of the person you want to find: ")
+    name_to_search = input("Enter the Name of the person you want to find:(Sn, Fn M.I) ")
     found = False
     with open("./python_to_txtfile.txt", "r") as file_handle:
         print(f"\nSearching for '{name_to_search}' in the file...")
@@ -12,7 +12,9 @@ def search_in_file():
     for i in range(0, len(lines), 8):
         if name_to_search.lower() in lines[i].lower():
             found = True 
+            print(f"\nInformation under '{name_to_search}': ")
             print("".join(lines[i:i+8]))
+            break
 
     if not found: 
         print("Not Found!")
